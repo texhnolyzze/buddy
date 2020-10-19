ababbf3873bf400eecf206c74d5299b21baf43cd
ikarimullin <habibula>
2020-10-20 00:32:59 +0300

    more thorough testing

--- buddy.h
+++ buddy.h
@@ -7,25 +7,25 @@
 #endif //BUDDY_BUDDY_H
 
 typedef struct block {
-    uint8_t level;
-    uint32_t address;
-    struct block *next;
-    struct block *prev;
+  uint8_t level;
+  uint32_t address;
+  struct block *next;
+  struct block *prev;
 } block_t;
 
 typedef struct buddy_allocator {
-    uint32_t page_size;
-    uint8_t max_level;
-    block_t **level;
-    uint8_t *used_index;
+  uint32_t page_size;
+  uint8_t max_level;
+  block_t **level;
+  uint8_t *used_index;
 } buddy_allocator_t;
 
 static inline uint32_t block_size_at_level(uint32_t level, buddy_allocator_t *allocator) {
-    return (1UL << (allocator->max_level - level)) * allocator->page_size;
+  return (1UL << (allocator->max_level - level)) * allocator->page_size;
 }
 
 static inline uint32_t block_size(block_t *block, buddy_allocator_t *allocator) {
-    return block_size_at_level(block->level, allocator);
+  return block_size_at_level(block->level, allocator);
 }
 
 buddy_allocator_t *buddy_allocator_create(uint32_t max_level, uint32_t page_size_order);
--- test.c
+++ test.c
@@ -1,62 +1,91 @@
 #include <assert.h>
 #include <stdlib.h>
 #include <time.h>
+#include <memory.h>
+#include <stdio.h>
 #include "buddy.h"
 
+typedef struct list_node {
+  struct list_node *next;
+  void *elem;
+} list_node_t;
+
 int main() {
-    uint32_t max_level = 19;
-    uint32_t page_size_order = 12;
-    uint32_t max_blocks = 1UL << max_level;
-    buddy_allocator_t *allocator = buddy_allocator_create(max_level, page_size_order);
-    block_t **blocks = calloc(max_blocks, sizeof(void *));
-    for (uint32_t i = 0; i < max_blocks; i++) {
-        block_t *block = balloc(allocator, 0);
-        blocks[i] = block;
-        assert(block);
-    }
-    for (uint32_t i = 0; i <= max_level; i++) {
-        assert(balloc(allocator, i) == NULL);
-    }
-    for (uint32_t i = 0; i < max_blocks; i++) {
-        bfree(allocator, blocks[i]);
-    }
-    block_t *block = balloc(allocator, max_level);
-    assert(block);
-    assert(block->address == 0);
-    assert(block->level == 0);
-    assert(!balloc(allocator, max_level));
-    bfree(allocator, block);
-    block = balloc(allocator, 1);
-    assert(block);
-    bfree(allocator, block);
-    block = balloc(allocator, max_level);
-    assert(block);
-    assert(block->address == 0);
-    bfree(allocator, block);
-    uint32_t insert_pos = 0;
-    srand(time(NULL));
-//  Test for segfaults
-    for (uint32_t i = 0; i < 100000; i++) {
-        if (rand() % 2) {
-            block = balloc(allocator, i % (max_level + 1));
-            if (block) {
-                assert(block->level >= 0 && block->level <= max_level);
-                assert(!block->next);
-                assert(!block->prev);
-                assert(block->address % allocator->page_size == 0);
-                blocks[insert_pos] = block;
-                insert_pos = (insert_pos + 1) % max_blocks;
-            }
-        } else {
-            for (uint32_t j = 0; j < max_blocks; j++) {
-                if (blocks[j]) {
-                    block = blocks[j];
-                    bfree(allocator, block);
-                    blocks[j] = NULL;
-                    break;
-                }
-            }
-        }
-    }
-    return 0;
+  uint32_t max_level = 15;
+  uint32_t page_size_order = 5;
+  uint32_t max_blocks = 1UL << max_level;
+  buddy_allocator_t *allocator = buddy_allocator_create(max_level, page_size_order);
+  block_t **blocks = calloc(max_blocks, sizeof(void *));
+  for (uint32_t i = 0; i < max_blocks; i++) {
+	block_t *block = balloc(allocator, 0);
+	blocks[i] = block;
+	assert(block);
+  }
+  for (uint32_t i = 0; i <= max_level; i++) {
+	assert(balloc(allocator, i) == NULL);
+  }
+  for (uint32_t i = 0; i < max_blocks; i++) {
+	bfree(allocator, blocks[i]);
+  }
+  block_t *block = balloc(allocator, max_level);
+  assert(block);
+  assert(block->address == 0);
+  assert(block->level == 0);
+  assert(!balloc(allocator, max_level));
+  bfree(allocator, block);
+  list_node_t *head = NULL;
+  list_node_t *tail = NULL;
+  memset(blocks, 0, max_blocks * sizeof(void *));
+  srand(time(NULL));
+  uint32_t list_size = 0;
+  uint8_t free_all = 0;
+//Emulate workload
+  for (uint32_t i = 0; i < 10000000; i++) {
+	double r = (double)rand() / (double)RAND_MAX;
+	if (!free_all && r < 0.75) { // 75% allocations
+	  uint32_t order = rand() % (max_level + 1);
+	  block = balloc(allocator, order);
+	  if (block) {
+		assert(!block->next);
+		assert(!block->prev);
+		assert(block->level == (max_level - order));
+		assert(block->address % allocator->page_size == 0);
+		assert(!blocks[block->address / allocator->page_size]);
+		blocks[block->address / allocator->page_size] = block;
+		if (!tail) {
+		  head = tail = calloc(1, sizeof(list_node_t));
+		} else {
+		  tail->next = calloc(1, sizeof(list_node_t));
+		  tail = tail->next;
+		}
+		tail->elem = block;
+		list_size++;
+	  } else {
+		assert(head);
+		free_all = 1;
+	  }
+	} else {
+	  if (head) {
+		block = head->elem;
+//		assert(!block->next); fails sometimes, can't figure out why
+		assert(!block->prev);
+		assert(block->level >= 0 && block->level <= max_level);
+		assert(block->address % allocator->page_size == 0);
+		assert(blocks[block->address / allocator->page_size]);
+		blocks[block->address / allocator->page_size] = NULL;
+		list_node_t *temp = head;
+		head = head->next;
+		if (!head)
+		  tail = NULL;
+		free(temp);
+		bfree(allocator, block);
+		list_size--;
+	  } else {
+		free_all = 0;
+	  }
+	}
+	if (i % 1000 == 0)
+	  printf("%u\n", list_size);
+  }
+  return 0;
 }
